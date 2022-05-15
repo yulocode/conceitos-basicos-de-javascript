2e8fd4f58c80d308206c6d897e33388ed268c22b
yulocode <101001858+yulocode@users.noreply.github.com>
2022-05-14 21:51:56 -0300

    Add files via upload

---
+++ algunscuidados.js
@@ -0,0 +1,12 @@
+console.log("10" / 2)
+console.log("show!", 10)
+
+let macaco = 10 
+
+macaco = 20 
+
+macaco = "eu sou o mior"
+
+console.log(macaco)
+
+console.log((10.345.toFixed(2)))
---
+++ var.js
@@ -0,0 +1,11 @@
+var a = 3
+let b = 4
+
+a = 300
+b = 400
+
+console.log(a, b)
+
+const c = 5
+// c = 50
+console.log(c)
