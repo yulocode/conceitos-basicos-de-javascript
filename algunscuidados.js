b394346d260e3c0c804f18d4b0d9ee0a8e846bdd
yulocode <yulocode@gmail.com>
2022-06-01 20:45:50 -0300

    Modificações no projeto

--- algunscuidados.js
+++ algunscuidados.js
@@ -1,12 +1,12 @@
 console.log("10" / 2)
 console.log("show!", 10)
 
-let macaco = 10 
+let humilde = 10 
 
-macaco = 20 
+humilde = 20 
 
-macaco = "eu sou o mior"
+humilde = "eu sou o mior"
 
-console.log(macaco)
+console.log(humilde)
 
 console.log((10.345.toFixed(2)))
