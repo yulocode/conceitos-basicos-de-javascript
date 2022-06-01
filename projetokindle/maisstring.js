34183c3ab7e0b7afed3f539a6c1da62c1db1a11b
yulocode <yulocode@gmail.com>
2022-06-01 20:53:59 -0300

    string

---
+++ projetokindle/maisstring.js
@@ -0,0 +1,12 @@
+let str = 'Teste';
+
+console.log(str.length);
+console.log(str.toUpperCase());
+console.log(str.toLowerCase());
+str = '1';
+
+console.log(parseInt(str));
+
+str = '1.5';
+
+console.log(parseFloat(str));
